62acb7f906ab500d7c658293105b891f7324b984
Mohamed Baddi <project@baddi.info>
2022-02-08 14:24:32 +0100

    Rebase

--- client/src/settings.ts
+++ client/src/settings.ts
@@ -1,3 +1,4 @@
 export const SERVER_HOST = process.env.SERVER_HOST || "localhost";
 export const SERVER_PORT = process.env.SERVER_PORT || "3000";
 export const PUBLIC_URL = process.env.PUBLIC_URL || `http://${SERVER_HOST}:${SERVER_PORT}`;
+export const BASE_NAME = process.env.BASE_NAME || `/`;
