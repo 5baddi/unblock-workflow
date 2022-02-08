f0a1b3f3603804541b0fc407f6153957b9d5a7cc
Mohamed Baddi <project@baddi.info>
2022-02-08 11:38:14 +0100

    Update removing service worker

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
-import registerServiceWorker from "./registerServiceWorker";
+// import registerServiceWorker from "./registerServiceWorker";
 import App from "./components/app";
 
 ReactDOM.render(<App/>, document.getElementById("root"));
 
+// TODO: Enable service worker for assets loading optimization
 // registerServiceWorker();
