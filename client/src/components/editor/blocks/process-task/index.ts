b8a9c6090b488f20f85e61c7cb7b7e629dfb52aa
Mohamed Baddi <project@baddi.info>
2022-02-10 12:57:39 +0100

    Update process-task block icon

--- client/src/components/editor/blocks/process-task/index.ts
+++ client/src/components/editor/blocks/process-task/index.ts
@@ -1,10 +1,59 @@
-import { NodeBlock, tripetto } from "tripetto";
-// import * as ICON from "./icon.svg";
+import { NodeBlock, tripetto, editor, Forms, slots } from "tripetto";
+import ICON  from "./assets/icon.svg";
+
+const BLOCK_NAME = "process-task";
 
 @tripetto({
     type: "node",
-    identifier: "process-task",
-    icon: "",
-    label: "Process Task"
+    identifier: BLOCK_NAME,
+    icon: ICON,
+    alias: BLOCK_NAME,
+    get label() {
+        // TODO: use translation
+        return "Process task";
+    }
 })
-export class ProcessTask extends NodeBlock {}
+export class ProcessTask extends NodeBlock
+{
+    @slots
+    defineSlot(): void {
+        // this.slots.feature({
+        //     type: Slots.String,
+        //     reference: "name",
+        //     label: "Name",
+        //     default: "Unnamed process task",
+        //     exportable: false,
+        // });
+    }
+
+    @editor
+    defineEditor() {
+        this.editor.form({
+            title: "Explanation",
+            controls: [
+                new Forms
+                    .Static(
+                        "Generate a process based on already defined process tasks.."
+                    )
+                    .markdown(),
+            ],
+        });
+
+        this.editor.name(false, false, "Name", false);
+
+        // this.editor.option({
+        //     name: "Name",
+        //     form: {
+        //         title: "Name",
+        //         controls: [
+        //             new Forms.Text("singleline")
+        //                 .placeholder("Unnamed process task")
+        //                 .action("@", insertVariable(this))
+        //                 .autoFocus()
+        //                 .autoSelect(),
+        //         ]
+        //     },
+        //     locked: true
+        // });
+    }
+}
