725afa804621bfdd7e6823a8de23629f037a16d2
Mohamed Baddi <project@baddi.info>
2022-02-19 17:47:56 +0100

    Init v0.0.9

--- client/src/settings.ts
+++ client/src/settings.ts
@@ -3,4 +3,4 @@ export const SERVER_HOST = process.env.SERVER_HOST || "localhost";
 export const SERVER_PORT = process.env.SERVER_PORT || "3000";
 export const PUBLIC_URL = process.env.PUBLIC_URL || `http://${SERVER_HOST}:${SERVER_PORT}`;
 export const BASE_NAME = process.env.BASE_NAME || `/`;
-export const VERSION = "0.0.8";
+export const VERSION = "0.0.9";
