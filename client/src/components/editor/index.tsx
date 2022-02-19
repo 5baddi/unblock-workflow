4687404036702d164f40900cea9346724ab6aef8
Mohamed Baddi <project@baddi.info>
2022-02-19 16:25:06 +0100

    Fix typo

--- client/src/components/editor/index.tsx
+++ client/src/components/editor/index.tsx
@@ -551,11 +551,9 @@ class Editor extends React.Component<IEditorProps, IEditorState>
     {
         let endpoint = `${PUBLIC_URL}/api/definitions`;
         let user = this.state.user;
-        console.log(user);
 
-        if (user && typeof user.tenantId === "string" && typeof user.Id === "string") {
-            endpoint = endpoint.concat(`/${user.tenantId}/${user.Id}`);
-            console.log(endpoint);
+        if (user && typeof user.tenantId === "string" && typeof user.id === "string") {
+            endpoint = endpoint.concat(`/${user.tenantId}/${user.id}`);
         }
 console.log(endpoint);
         return API.get(endpoint)
