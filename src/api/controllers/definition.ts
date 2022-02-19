3c8adde7475231c8f3966cd2a46d4b7986f701be
Mohamed Baddi <project@baddi.info>
2022-02-19 17:05:33 +0100

    Remove debug

--- src/api/controllers/definition.ts
+++ src/api/controllers/definition.ts
@@ -20,7 +20,7 @@ function index(request, response)
         filter.tenant_id = tenantId;
         filter.user_id = tenantId;
     }
-console.log(filter);
+
     return connect()
         .then(client => {
             let db = client.db();
@@ -32,7 +32,7 @@ console.log(filter);
                 .then(items => {
 
                     client.close();
-console.log(items);
+
                     return response.send({ success: true, definitions: items });
                 })
                 .catch(error => {
