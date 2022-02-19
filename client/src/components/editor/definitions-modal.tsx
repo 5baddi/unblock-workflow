bc2a64fdc7314ac13961188bbbaedca0e53bcda4
Mohamed Baddi <project@baddi.info>
2022-02-19 15:23:10 +0100

    Only load definitions when modal opened

--- client/src/components/editor/definitions-modal.tsx
+++ client/src/components/editor/definitions-modal.tsx
@@ -26,9 +26,9 @@ class DefinitionsModal extends React.Component<IEditorDefinitionsModalProps, IEd
         this.loadDefinitions();
     }
 
-    componentDidUpdate(prevProps, prevState)
+    componentDidUpdate()
     {
-        if (prevProps.show !== this.props.show) {
+        if (this.props.show === true) {
             this.loadDefinitions();
         }
     }
