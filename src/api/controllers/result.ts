19f556c9c4d3535bfbb5768b8e38390c95ab55a5
Mohamed Baddi <project@baddi.info>
2022-02-22 16:21:20 +0100

    Revert "Revert calling webhook from backend"
    This reverts commit 4e87fe098a0edb201b0cd04363c9d57d5d4f8308.

--- src/api/controllers/result.ts
+++ src/api/controllers/result.ts
@@ -1,8 +1,60 @@
 import { ObjectId } from "mongodb";
-import { DEFINITION_COLLECTION_NAME, RESPONSE_COLLECTION_NAME } from '../../settings';
+import { DEFINITION_COLLECTION_NAME, RESPONSE_COLLECTION_NAME, RESULT_WEBHOOK } from '../../settings';
 import { connect } from "../../services/mongodb";
 import { v4 as uuidv4 } from 'uuid';
 import { IResponse, IDefinition } from '../../interfaces/definition';
+import axios from "axios";
+import https from "https";
+
+function storeResult(db, client, _response, id, fields, request, response)
+{
+    return db.collection(RESPONSE_COLLECTION_NAME)
+        .insertOne(JSON.parse(JSON.stringify(_response)))
+        .then(insertedResponse => {
+            client.close();
+
+            let query = "INSERT INTO `results` (`id`, `definition_id`, `key`, `type`, `version`, `data_type`, `name`, `value`, `snapshot`, `updated_at`, `created_at`) VALUES ?";
+            let key = uuidv4();
+
+            let rows = Object.values(fields).map((field) => {
+                let data = JSON.parse(JSON.stringify(field));
+
+                return [
+                    uuidv4(), 
+                    id,
+                    key,
+                    data.type,
+                    data.version,
+                    data.datatype,
+                    data.name,
+                    data.value,
+                    JSON.stringify(data),
+                    null,
+                    new Date(),
+                ];
+            });
+
+            return request.getConnection((conErr, connection) => {
+                if (conErr) {
+                    return response.status(500).send({
+                        success: false,
+                        message: conErr.message  || "failed to save result.",
+                    });
+                }
+
+                connection.query(query, [rows], (err) => {
+                    if (err) {
+                        return response.status(500).send({
+                            success: false,
+                            message: err.message  || "failed to save result.",
+                        });
+                    }
+
+                    return response.send({ success: true});
+                });
+            });
+        });
+}
 
 function save(request, response)
 {
@@ -48,52 +100,16 @@ function save(request, response)
                         _response.tenants_ids = definition.tenants_ids;
                         _response.created_at = new Date();
 
-                        return db.collection(RESPONSE_COLLECTION_NAME)
-                            .insertOne(JSON.parse(JSON.stringify(_response)))
-                            .then(insertedResponse => {
-                                client.close();
+                        if (RESULT_WEBHOOK) {
+                            let httpsAgent = new https.Agent({ rejectUnauthorized: false });
 
-                                let query = "INSERT INTO `results` (`id`, `definition_id`, `key`, `type`, `version`, `data_type`, `name`, `value`, `snapshot`, `updated_at`, `created_at`) VALUES ?";
-                                let key = uuidv4();
-
-                                let rows = Object.values(fields).map((field) => {
-                                    let data = JSON.parse(JSON.stringify(field));
-
-                                    return [
-                                        uuidv4(), 
-                                        id,
-                                        key,
-                                        data.type,
-                                        data.version,
-                                        data.datatype,
-                                        data.name,
-                                        data.value,
-                                        JSON.stringify(data),
-                                        null,
-                                        new Date(),
-                                    ];
+                            return axios.post(RESULT_WEBHOOK, { httpsAgent: httpsAgent, data: _response })
+                                .then(webhookResponse => {
+                                    return storeResult(db, client, _response, id, fields, request, response);
                                 });
+                        }
 
-                                return request.getConnection((conErr, connection) => {
-                                    if (conErr) {
-                                        return response.status(500).send({
-                                            success: false,
-                                            message: conErr.message  || "failed to save result.",
-                                        });
-                                    }
-
-                                    connection.query(query, [rows], (err) => {
-                                        if (err) {
-                                            return response.status(500).send({
-                                                success: false,
-                                                message: err.message  || "failed to save result.",
-                                            });
-                                        }
-
-                                        return response.send({ success: true});
-                                    });
-                                });
-                            });
+                        return storeResult(db, client, _response, id, fields, request, response);
                     })
                     .catch(error => {
                         client.close();
