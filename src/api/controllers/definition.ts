d44bff3a4f06e017cb8b29d77326d23e3152f7b6
Mohamed Baddi <project@baddi.info>
2022-02-23 12:12:00 +0100

    Undo filter by user

--- src/api/controllers/definition.ts
+++ src/api/controllers/definition.ts
@@ -21,9 +21,10 @@ function index(request, response)
         filter.tenant_id = tenantId;
     }
     
-    if (typeof userId === "string") {
-        filter.user_id = userId;
-    }
+    // TODO: filter by user if they don't have permission to see all
+    // if (typeof userId === "string") {
+    //     filter.user_id = userId;
+    // }
 
     return connect()
         .then(client => {
