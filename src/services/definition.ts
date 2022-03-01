d05a5e24f6d40c14a1ca94bcc6e2ddaf2154cf35
Mohamed Baddi <project@baddi.info>
2022-03-01 17:38:57 +0100

    Filter process task blocks

--- src/services/definition.ts
+++ src/services/definition.ts
@@ -27,28 +27,40 @@ async function loadSubDefinitions(db, definition: IDefinition): Promise<IDefinit
 
     return Promise.all(
             definition.clusters.map(async(cluster) => {
-                clusters.push(cluster);
-
                 if (! cluster.nodes || cluster.nodes.length === 0) {
+                    clusters.push(cluster);
+
                     return;
                 }
 
-                return await Promise.all(cluster.nodes.map(async(node) => {
+                return await Promise.all(cluster.nodes.map(async(node, index) => {
                     if (
                         ! node.block || typeof node.block.type !== "string" 
                         || node.block.type !== "process-task"
                         || typeof node.definitionId !== "string"
                         || node.definitionId === ""
                     ) {
+                        clusters.push(cluster);
+
                         return;
                     }
 
                     let result = await db.collection(DEFINITION_COLLECTION_NAME).findOne({ _id: new ObjectId(<string> node.definitionId) });
                     if (! result) {
+                        if (typeof cluster !== "undefined" && Array.isArray(cluster.nodes) && cluster.nodes[index]) {
+                            cluster.nodes.splice(index, 1);
+                        }
+
                         return;
                     }
 
                     let subDefinition: IDefinition = Object.assign({} as IDefinition, result);
+
+                    if (typeof cluster !== "undefined" && Array.isArray(cluster.nodes) && cluster.nodes[index]) {
+                        cluster.nodes.splice(index, 1);
+                    }
+
+                    clusters.push(cluster);
                     clusters.push(...subDefinition.clusters);
                 }));
             })
