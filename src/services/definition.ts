a2c5238472cd0b9732ecf4a3309643a09fffe89a
Mohamed Baddi <project@baddi.info>
2022-03-02 16:04:21 +0100

    Improve loading sub definitions

--- src/services/definition.ts
+++ src/services/definition.ts
@@ -27,9 +27,9 @@ async function loadSubDefinitions(db, definition: IDefinition): Promise<IDefinit
 
     return Promise.all(
             definition.clusters.map(async(cluster) => {
-                if (! cluster.nodes || cluster.nodes.length === 0) {
-                    clusters.push(cluster);
+                clusters.push(cluster);
 
+                if (! cluster.nodes || cluster.nodes.length === 0) {
                     return;
                 }
 
@@ -37,33 +37,18 @@ async function loadSubDefinitions(db, definition: IDefinition): Promise<IDefinit
                     if (
                         ! node.block || typeof node.block.type !== "string" 
                         || node.block.type !== "process-task"
-                        || typeof node.definitionId !== "string"
+                        || typeof node.block.definitionId !== "string" || node.block.definitionId === ""
                     ) {
-                        if (typeof cluster !== "undefined" && Array.isArray(cluster.nodes) && cluster.nodes[index] && node.definitionId === "") {
-                            cluster.nodes.splice(index, 1);
-                        }
-
-                        clusters.push(cluster);
-
                         return;
                     }
 
-                    let result = await db.collection(DEFINITION_COLLECTION_NAME).findOne({ _id: new ObjectId(<string> node.definitionId) });
+                    let result = await db.collection(DEFINITION_COLLECTION_NAME).findOne({ _id: new ObjectId(<string> node.block.definitionId) });
                     if (! result) {
-                        if (typeof cluster !== "undefined" && Array.isArray(cluster.nodes) && cluster.nodes[index]) {
-                            cluster.nodes.splice(index, 1);
-                        }
-
                         return;
                     }
 
                     let subDefinition: IDefinition = Object.assign({} as IDefinition, result);
 
-                    if (typeof cluster !== "undefined" && Array.isArray(cluster.nodes) && cluster.nodes[index]) {
-                        cluster.nodes.splice(index, 1);
-                    }
-
-                    clusters.push(cluster);
                     clusters.push(...subDefinition.clusters);
                 }));
             })
