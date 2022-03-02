1824d44dd5f1ab640329ad406ae6ffba656564e2
Mohamed Baddi <project@baddi.info>
2022-03-02 16:45:49 +0100

    Remove process task block

--- src/services/definition.ts
+++ src/services/definition.ts
@@ -27,9 +27,9 @@ async function loadSubDefinitions(db, definition: IDefinition): Promise<IDefinit
 
     return Promise.all(
             definition.clusters.map(async(cluster) => {
-                clusters.push(cluster);
-
                 if (! cluster.nodes || cluster.nodes.length === 0) {
+                    clusters.push(cluster);
+
                     return;
                 }
 
@@ -39,16 +39,28 @@ async function loadSubDefinitions(db, definition: IDefinition): Promise<IDefinit
                         || node.block.type !== "process-task"
                         || typeof node.block.definitionId !== "string" || node.block.definitionId === ""
                     ) {
+                        if (node.block && typeof node.block.type === "string" && node.block.type === "process-task" && typeof node.block.definitionId === "string" && node.block.definitionId === "") {
+                            cluster.nodes?.splice(index, 1);
+                        }
+
+                        clusters.push(cluster);
+
                         return;
                     }
 
                     let result = await db.collection(DEFINITION_COLLECTION_NAME).findOne({ _id: new ObjectId(<string> node.block.definitionId) });
                     if (! result) {
+                        cluster.nodes?.splice(index, 1);
+                        clusters.push(cluster);
+
                         return;
                     }
 
                     let subDefinition: IDefinition = Object.assign({} as IDefinition, result);
 
+                    cluster.nodes?.splice(index, 1);
+
+                    clusters.push(cluster);
                     clusters.push(...subDefinition.clusters);
                 }));
             })
