0e24d5018c3ab120daf622acf996dee124306be7
Mohamed Baddi <project@baddi.info>
2022-03-07 17:54:56 +0100

    Revert "Builder webhook on saving"
    This reverts commit 10f86e2826624267a4d17c9525a53af77ce7f4bb.

--- src/api/controllers/definition.ts
+++ src/api/controllers/definition.ts
@@ -1,10 +1,9 @@
 import { ObjectId } from "mongodb";
-import { DEFINITION_COLLECTION_NAME, ROOT_USER_ID, SNAPSHOT_COLLECTION_NAME, SUPPORTED_VERSION, BUILDER_ON_SAVE_WEBHOOK } from '../../settings';
+import { DEFINITION_COLLECTION_NAME, ROOT_USER_ID, SNAPSHOT_COLLECTION_NAME, SUPPORTED_VERSION } from '../../settings';
 import { IDefinition, ISnapshot } from "../../interfaces/definition";
 import { IMongoDBFilter } from '../../interfaces';
 import { connect } from "../../services/mongodb";
 import { generateHash } from "../../helpers";
-import * as Superagent from "superagent";
 
 function checkDefinitionVersion(definition: IDefinition): boolean
 {
@@ -220,15 +219,6 @@ function save(request, response)
 
                     let definition = Object.assign({} as IDefinition, result.value);
                     definition.is_saved = true;
-
-                    if (typeof BUILDER_ON_SAVE_WEBHOOK !== "undefined") {
-                        return Superagent
-                            .post(BUILDER_ON_SAVE_WEBHOOK)
-                            .send(definition)
-                            .then((webhookResult) => {
-                                return response.send({ success: true, definition });
-                            });
-                    }
 
                     return response.send({ success: true, definition });
                 })
