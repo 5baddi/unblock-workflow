9363e84f36086ea1eefaa6c28312c03929c289fc
Mohamed Baddi <project@baddi.info>
2022-03-18 18:00:40 +0100

    Disable runner outside glue

--- client/src/pages/run/index.tsx
+++ client/src/pages/run/index.tsx
@@ -3,12 +3,17 @@ import { useParams } from "react-router-dom";
 import { Grid }  from "@mui/material";
 import { ChatRunner as UnblockChatRunner } from "../../components/runner/chat";
 import { GlueContext } from '@glue42/react-hooks';
+import { ENV } from '../../../../src/settings';
 
 export default function ChatRunner ()
 {
     let { id } = useParams();
 
     const glue = React.useContext(GlueContext);
+
+    if (window.location === window.parent.location && ENV !== "development") {
+        return (<h1>Not found</h1>);
+    }
 
     return (
         <Grid container>
