fa73498e8a57af2c27f6da0c03d4693c3af1d471
Mohamed Baddi <project@baddi.info>
2022-03-22 10:11:41 +0100

    Rafactoring

--- src/services/definition.ts
+++ src/services/definition.ts
@@ -1,16 +1,72 @@
-import { IDefinition, ISnapshot } from '../interfaces/definition';
+import { IDefinition, ISnapshot } from "../interfaces/definition";
 import { connect } from "./mongodb";
-import { DEFINITION_COLLECTION_NAME, DEFAULT_MONGODB_DATABASE, NORMALIZED_RESPONSE_COLLECTION_NAME, SNAPSHOT_COLLECTION_NAME, SUPPORTED_VERSION, ROOT_USER_ID, ENV } from '../settings';
-import { ObjectId } from 'mongodb';
-import { ICluster } from '@tripetto/map';
-import { IMongoDBFilter } from '../interfaces/index';
+import { DEFINITION_COLLECTION_NAME, DEFAULT_MONGODB_DATABASE, NORMALIZED_RESPONSE_COLLECTION_NAME, SNAPSHOT_COLLECTION_NAME, SUPPORTED_VERSION, ROOT_USER_ID, ENV } from "../settings";
+import { Db, ObjectId } from "mongodb";
+import { ICluster, INode } from "@tripetto/map";
+import { IMongoDBFilter } from "../interfaces";
 
 function checkDefinitionVersion(definition: IDefinition): boolean
 {
     return definition.builder.version >= SUPPORTED_VERSION;
 }
 
-async function loadSubDefinitions(db, definition: IDefinition): Promise<IDefinition>
+function isProcessTaskBlock(node: INode): boolean
+{
+    if (
+        ! node.block || typeof node.block.type !== "string" 
+        || node.block.type !== "process-task"
+        || typeof node.block.definitionId !== "string" || node.block.definitionId === ""
+    ) {
+        return false;
+    }
+
+    return true;
+}
+
+async function loadSubDefinition(db: Db, definitionId: string): Promise<IDefinition | undefined>
+{
+    let result = await db.collection(DEFINITION_COLLECTION_NAME).findOne({ _id: new ObjectId(definitionId) });
+    if (! result) {
+        return undefined;
+    }
+
+    let subDefinition: IDefinition = Object.assign({} as IDefinition, result);
+
+    return subDefinition;
+}
+
+async function loadSubClusters(db: Db, cluster: ICluster): Promise<ICluster>
+{
+    let _cluster = Object.assign({}, JSON.parse(JSON.stringify(cluster)));
+
+    if (typeof _cluster.nodes !== "undefined" && _cluster.nodes.length > 0) {
+        await Promise.all(_cluster.nodes.map(async(node, index) => {
+            if (! isProcessTaskBlock(node)) {
+                return;
+            }
+
+            let subDefinition: IDefinition | undefined = await loadSubDefinition(db, <string> node.block.definitionId);
+            if (typeof subDefinition === "undefined") {
+                return;
+            }
+
+            subDefinition.clusters.map(function (subCluster) {
+                let _index = index + 1;
+
+                _cluster.nodes?.splice(_index, 0, ...subCluster.nodes ?? []);
+                if (! Array.isArray(_cluster.branches)) {
+                    _cluster.branches = [];
+                }
+
+                _cluster.branches?.push(...subCluster.branches ?? []);
+            });
+        }));
+    }
+
+    return _cluster as ICluster;
+}
+
+async function loadSubDefinitions(db: Db, definition: IDefinition): Promise<IDefinition>
 {
     if (! Array.isArray(definition.clusters)) {
         return Promise.resolve(definition);
@@ -20,36 +76,7 @@ async function loadSubDefinitions(db, definition: IDefinition): Promise<IDefinit
 
     return Promise.all(
             definition.clusters.map(async(cluster) => {
-                let _cluster = Object.assign({}, JSON.parse(JSON.stringify(cluster)));
-
-                if (typeof _cluster.nodes !== "undefined" && _cluster.nodes.length > 0) {
-                    await Promise.all(_cluster.nodes.map(async(node, index) => {
-                        if (
-                            ! node.block || typeof node.block.type !== "string" 
-                            || node.block.type !== "process-task"
-                            || typeof node.block.definitionId !== "string" || node.block.definitionId === ""
-                        ) {
-                            return;
-                        }
-    
-                        let result = await db.collection(DEFINITION_COLLECTION_NAME).findOne({ _id: new ObjectId(<string> node.block.definitionId) });
-                        if (! result) {
-                            return;
-                        }
-    
-                        let subDefinition: IDefinition = Object.assign({} as IDefinition, result);
-                        subDefinition.clusters.map(function (subCluster) {
-                            let _index = index + 1;
- 
-                            _cluster.nodes?.splice(_index, 0, ...subCluster.nodes ?? []);
-                            if (! Array.isArray(_cluster.branches)) {
-                                _cluster.branches = [];
-                            }
-
-                            _cluster.branches?.push(...subCluster.branches ?? []);
-                        });
-                    }));
-                }
+                let _cluster = await loadSubClusters(db, cluster);
 
                 clusters.push(_cluster as ICluster);
             })
